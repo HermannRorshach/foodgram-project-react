7749a551dc69a277bb5948f7527f90996769908b
Pavel Borisov <perevod-avtozavod@yandex.ru>
2023-09-20 00:22:21 +0200

    Реализация `users`
    - Приложение реализовано
    - Удалены ненужные файлы

--- backend/users/admin.py
+++ backend/users/admin.py
@@ -1,3 +1,31 @@
 from django.contrib import admin
 
-# Register your models here.
+from users.models import Subscription, User
+
+admin.site.unregister(User)
+
+
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+    list_display = (
+        'username',
+        'email',
+        'first_name',
+        'last_name'
+    )
+    search_fields = (
+        'username',
+        'email'
+    )
+    list_filter = (
+        'first_name',
+        'last_name'
+    )
+
+
+@admin.register(Subscription)
+class SubscriptionAdmin(admin.ModelAdmin):
+    list_display = (
+        'user',
+        'author'
+    )
--- backend/users/models.py
+++ backend/users/models.py
@@ -1,3 +1,30 @@
+from django.contrib.auth import get_user_model
 from django.db import models
 
-# Create your models here.
+User = get_user_model()
+
+
+class Subscription(models.Model):
+    user = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        related_name='follower',
+        verbose_name='Подписчик'
+    )
+    author = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        related_name='following',
+        verbose_name='Автор'
+    )
+
+    class Meta:
+        verbose_name = 'Подписка'
+        verbose_name_plural = 'Подписки'
+        constraints = [models.UniqueConstraint(
+            fields=['user', 'author'],
+            name='unique_subscriptions'
+        )]
+
+    def __str__(self):
+        return f'{self.user} подписался на {self.author}'
