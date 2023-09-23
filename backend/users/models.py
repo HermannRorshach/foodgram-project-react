7acfa8f955344a5631095c4b51f629deb2bea655
Pavel Borisov <perevod-avtozavod@yandex.ru>
2023-09-23 20:43:56 +0200

    Небольшие улучшения

--- backend/users/models.py
+++ backend/users/models.py
@@ -1,4 +1,5 @@
 from django.contrib.auth import get_user_model
+from django.core.exceptions import ValidationError
 from django.db import models
 
 User = get_user_model()
@@ -25,6 +26,10 @@ class Subscription(models.Model):
             fields=['user', 'author'],
             name='unique_subscriptions'
         )]
+
+    def clean(self):
+        if self.user == self.author:
+            raise ValidationError("Подписка на себя запрещено!")
 
     def __str__(self):
         return f'{self.user} подписался на {self.author}'
