25d828f96fad4c46e16270f21a7506e266da5710
Pavel Borisov <perevod-avtozavod@yandex.ru>
2023-09-23 03:43:47 +0200

    Реализация `recipes`
    - Реализовано импорт данных

---
+++ backend/recipes/management/commands/load_models.py
@@ -0,0 +1,29 @@
+import csv
+import os
+
+from django.conf import settings
+from django.core.management.base import BaseCommand
+
+from recipes.models import Ingredient
+
+
+class Command(BaseCommand):
+    file_path = os.path.join(settings.BASE_DIR / 'data/ingredients.csv')
+
+    def handle(self, *args, **options):
+        try:
+            self._import_data()
+        except Exception as err:
+            self.stdout.write(self.style.ERROR(f'Ошибка при импорте: {err}'))
+        else:
+            self.stdout.write(self.style.SUCCESS('Данные загружены!'))
+
+    def _import_data(self):
+        with open(self.file_path, encoding='UTF-8') as f:
+            read_data = csv.reader(f)
+            next(read_data)
+            ingredients = [Ingredient(name=name,
+                                      measurement_unit=measurement_unit)
+                           for name, measurement_unit in read_data]
+
+            Ingredient.objects.bulk_create(ingredients)
